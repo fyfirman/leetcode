70890bf2e96435e28ff9a3375171570ba06c0ddd
Firmansyah Yanuar <fyfirman@gmail.com>
2023-08-26 23:52:30 +0700

    Time: 65 ms (82.52%) | Memory: 45.9 MB (60.58%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.ts
+++ 125-valid-palindrome/valid-palindrome.ts
@@ -1,6 +1,6 @@
 function isPalindrome(s: string): boolean {
     const transformed = s.toLowerCase().replace(/[^0-9a-z]/gi, '')
-    console.log(transformed)
+
     for(let i=0; i < transformed.length - 1; i++){
         if(transformed[i] !== transformed[transformed.length-1 - i]){
             return false
