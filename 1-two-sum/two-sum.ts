23cea0dbda02d27ae96fd63964bc98bf40bed8c2
Firmansyah Yanuar <fyfirman@gmail.com>
2023-08-27 02:10:36 +0700

    Time: 80 ms (50.12%) | Memory: 51.4 MB (5.02%) - LeetSync

--- 1-two-sum/two-sum.ts
+++ 1-two-sum/two-sum.ts
@@ -1,9 +1,37 @@
+/**
+    brute force
+    
+    time comp : O(n^2)
+ */
+
+// function twoSum(nums: number[], target: number): number[] {
+//     for(let i = 0; i < nums.length; i++){
+//         for(let j = i + 1; j < nums.length; j++){
+//             if(nums[i] + nums[j] === target){
+//                 return [i, j];
+//             }
+//         }
+//     }
+// };
+
+
+/**
+    hashmap complementary
+
+    time comp : O(n)
+ */
+
 function twoSum(nums: number[], target: number): number[] {
+    const map : Record<number, number> = {};
+
+    // build hashmap 
     for(let i = 0; i < nums.length; i++){
-        for(let j = i + 1; j < nums.length; j++){
-            if(nums[i] + nums[j] === target){
-                return [i, j];
-            }
+        Object.assign(map, {[target-nums[i]] : i})
+    }
+
+    for(let i = 0; i < nums.length; i++){
+        if(!!map[nums[i]] && i !== map[nums[i]]){
+            return [i, map[nums[i]]]
         }
     }
-};
+}
