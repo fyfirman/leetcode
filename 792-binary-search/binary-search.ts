da2725013cba52e46b6f188ded9481de4baa7276
Firmansyah Yanuar <fyfirman@gmail.com>
2023-09-22 00:34:26 +0700

    Time: 54 ms (84.92%) | Memory: 46 MB (37.58%) - LeetSync

---
+++ 792-binary-search/binary-search.ts
@@ -0,0 +1,20 @@
+function search(nums: number[], target: number): number {
+    let left = 0;
+    let right = nums.length - 1;
+    
+    while(left <= right){    
+        let mid = Math.floor((left + right) / 2);
+
+        if(nums[mid] === target){
+            return mid;
+        }
+
+        if(nums[mid] > target){
+            right = mid - 1;
+        } else {
+            left = mid + 1;
+        }
+    }
+
+    return -1;
+};
