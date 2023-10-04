7cee280c5f32f081d6f4582a8b84dd4281cbeaf6
Firmansyah Yanuar <fyfirman@gmail.com>
2023-10-04 11:08:34 +0700

    Time: 61 ms (47.42%) | Memory: 45.2 MB (90.02%) - LeetSync

--- 792-binary-search/binary-search.ts
+++ 792-binary-search/binary-search.ts
@@ -1,20 +1,48 @@
-function search(nums: number[], target: number): number {
-    let left = 0;
-    let right = nums.length - 1;
+/**
+  1. find exact value
+ */
+
+// function search(nums: number[], target: number): number {
+//     let left = 0;
+//     let right = nums.length - 1;
     
-    while(left <= right){    
-        let mid = Math.floor((left + right) / 2);
+//     while(left < right){    
+//         let mid = Math.floor((left + right) / 2);
 
-        if(nums[mid] === target){
-            return mid;
-        }
+//         if(nums[mid] === target){
+//             return mid;
+//         }
 
-        if(nums[mid] > target){
-            right = mid - 1;
+//         if(nums[mid] > target){
+//             right = mid - 1;
+//         } else {
+//             left = mid + 1;
+//         }
+//     }
+
+//     return -1;
+// };
+
+/**
+  2. find upper bound
+ */
+
+function search(nums: number[], target: number): number {
+    let left = 0, right = nums.length - 1;
+    
+    while(left < right){    
+        const mid = Math.floor((left + right) / 2);
+
+        if(nums[mid] >= target){
+            right = mid;
         } else {
             left = mid + 1;
         }
     }
 
-    return -1;
+    if(nums[left] !== target){
+      return -1;
+    }
+
+    return left;
 };
