10a974d3d3dcf19b99b0b47f12c14bbfa0bebb3a
Firmansyah Yanuar <fyfirman@gmail.com>
2023-10-21 01:14:01 +0700

    Time: 60 ms (58.33%) | Memory: 45 MB (33.33%) - LeetSync

---
+++ 2210-find-target-indices-after-sorting-array/find-target-indices-after-sorting-array.ts
@@ -0,0 +1,11 @@
+function targetIndices(nums: number[], target: number): number[] {
+  const sortedNums = nums.sort((a, b) => a - b);
+
+  const result = [];
+  for(let i=0; i < nums.length; i++){
+    if(sortedNums[i] === target){
+      result.push(i)
+    }
+  }
+  return result;  
+};
