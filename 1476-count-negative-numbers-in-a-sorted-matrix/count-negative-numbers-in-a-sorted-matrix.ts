4aca98a230656274e9861e19d46b7a49c961b3d7
Firmansyah Yanuar <fyfirman@gmail.com>
2023-10-21 00:56:03 +0700

    Time: 63 ms (26.92%) | Memory: 45.1 MB (55.13%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.ts
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.ts
@@ -1,31 +1,32 @@
+/**
+  key idea: find first negative number
+
+  1. loop array - O(n)
+  2. binary search - find first negative number index
+  3. check the index result is actual negative number
+  4. if yes, count the result
+ */
+
 function countNegatives(grid: number[][]): number {
   let result = 0;
-  for(let i=0; i < grid.length; i++){
-    const currentArray = grid[i];
 
-    // find first negative number
+  for(let i=0; i < grid.length; i++){
     let left = 0;
-    let right = currentArray.length - 1;
+    let right = grid[i].length - 1;
 
     while(left < right){
       let mid = Math.floor((left + right) / 2);
 
-      if(currentArray[mid] >= 0){
+      if(grid[i][mid] >= 0){
         left = mid + 1;
       } else {
         right = mid;
       }
     }
 
-    if(currentArray[left] < 0){
-      result += currentArray.length-left;
+    if(grid[i][left] < 0){
+      result += grid[i].length - left;
     }
-    // console.log({
-    //   i,
-    //   left,
-    //   right,
-    //   countNegative: currentArray[left] !== 0 ? currentArray.length-left : 0
-    // })
   }
 
   return result;
